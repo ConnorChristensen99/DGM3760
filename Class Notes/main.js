cec63c769c121a7c101abedee379e296d018d08a
ConnorChristensen99 <connor.christensen@uvu.edu>
2022-09-22 16:35:59 -0600

    Created folder, added basic HTML back if I want to retry buttons

--- Class Notes/main.js
+++ Class Notes/main.js
@@ -120,12 +120,23 @@ let gryfindorWizardNames = wizards.filter(wizard => wizard.house ==="Gryfindor")
 
 //look at makeTree - Advanced Functions Assignment //Recursive Functions will be needed
 function makeTree(arr, parent) {
-    let node = {}
+    let node = {};
 
-    return {}
+    let filteredArray = arr.filter((item) => { 
+        return item.parent === parent
+    })
+
+    filteredArray.forEach((item) => {
+        node[item.id] = makeTree(arr, item.id)
+    })
+
+
+    return node
 }
 
 
+
+////
 let myObj = {
     name: 'Connor'
 }
@@ -138,3 +149,213 @@ myObj.eyeColor = 'blue'//This adds that property with that value to the object
 let myNewProp = 'height'
 myObj.myNewProp = 4//THIS DOESN'T WORK /THIS WILL RETURN myNewProp:4
 myObj[myNewProp] = 5//THIS IS HOW YOU DO IT /THIS RESULTS IN Height:5
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+let todoUL = document.querySelector('.todoList'); 
+let todoInput = document.querySelector('#newInput')
+let addBtn = document.querySelector('#addBtn')
+
+addBtn.addEventListener('click', event => {
+    let todoText = todoInput.value
+    addTodo(todoText)
+    renderTodos(todos)
+})
+
+todoUL.addEventListener('click', (event) => {
+    console.log(event.target.dataset.todoid)
+    let todoID = event.target.dataset.todoid
+    completeTodo(todoID)
+    renderTodos(todos)
+})
+
+function completeTodo(id) {
+    let todoIdx = todos.findIndex(todo => todo.todoID === id)
+
+    todos[todoIdx].todoComplete = !todos[todoIdx].todoComplete
+}
+
+function addTodo(todoText) {
+    let newTodo = {
+        todoID: todos.length + 1,
+        todoText: todoText,
+        todoComplete: false
+    }
+    todos.push(newTodo)
+}
+
+function displayTodos(todos) {
+    todoUL.innerHTML = ""
+
+    todos.forEach(todo => {
+        let done = todo.todoComplete ? "done" : "";
+        let todoMarkup = `<li class="${done}" data-todoID = '${todo.todoID}'>
+                            ${todo.todoText}<span> <i class="fa fa-trash"></i></span>
+                            </li>`
+
+        todoUL.insertAdjacentHTML('beforeend', todoMarkup)
+    })
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//advanced functions copy and paste if i cant push it
+
+//RECURSION
+
+//Write a function sumTill(n) that will sum till n. (Must use Recursion)
+// example inputs and outputs
+// sumTill(1) //1
+// sumTill(2) // 2 + 1 = 3
+// sumTill(3) // 3 + 2 + 1 = 6
+// sumTill(100) // 100 + 99 + 98 ... + 2 + 1 = 5050
+// BONUS 1 point extra credit: There is a much simpler way to do this! (Hint: it is NOT a loop, or recursion) Write a function that is faster than recursion.
+
+function sumTill(n){
+    if (n <= 1){
+        return n;}
+        else {
+            return n + sumTill(n-1)
+        }
+
+}
+
+console.log(sumTill(5)); //15
+
+
+
+let data = [
+    { id: 'animals', parent: null },
+    { id: 'mammals', parent: 'animals' },
+    { id: 'cats', parent: 'mammals' },
+    { id: 'dogs', parent: 'mammals' },
+    { id: 'labrador', parent: 'dogs' },
+    { id: 'retreiver', parent: 'dogs' },
+    { id: 'corgi', parent: 'dogs' },
+    { id: 'persian', parent: 'cats' },
+    { id: 'siamese', parent: 'cats' },
+    { id: 'maineCoon', parent: 'cats' }
+];
+
+//write a function: makeTree(obj) that takes a flat data stucture, as seen above, and return a tree strucuture as seen below. Must use recursion.
+
+function makeTree(arr, parent) {
+    let node = {};
+
+    let filteredArray = arr.filter((item) => { 
+        return item.parent === parent
+    })
+
+    filteredArray.forEach((item) => {
+        node[item.id] = makeTree(arr, item.id)
+    })
+
+
+    return node
+}
+
+console.log('making tree')
+console.log(
+    //makes the output readable... Handy trick for outputing objects in the console.
+    JSON.stringify(
+        makeTree(data, null)
+        , null, 2
+    )
+)
+
+let reutrn = {
+    animals: {
+        mammals: {
+            dogs: {
+                labrador: {},
+                retreiver: {},
+                corgi: {},
+            },
+            cats: {
+                persian: {},
+                siamese: {},
+                maineCoon: {}
+            }
+        }
+    }
+}
+
+
+// SCOPES and CLOSURES
+
+//write 2 functions to be used as a filter for Array.filter()
+// Function between(a, b) should take two numbers and filter an array of numbers to only values between the function parameters
+//function inArr() should take an array of numbers and filter an array of numbers to ONLY numbers that are in the array passes to inArr()
+//Hint: both of these functions will return functions for the Array.filter() to use.
+
+
+function between(a, b){
+    let i = a;
+    let b = b;
+    
+    if (i <= b) {
+        return between(a++,b)
+    }
+}
+
+
+function inArr(arr) {
+
+}
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//uncomment out these lines when working on and turning in this section. They are currently commented out due to errors
+ console.log(arr.filter(between(2, 7))); // 2, 3, 4, 5, 6, 7
+
+ //console.log(arr.filter(inArr([4, 5, 7, 8, 9, 12, 14]))) // 4, 5, 7, 8, 9
+
+
+
+// REST PARAMETERS and SPREAD Syntax
+
+//write a function, sumAll() that can take any number of parameters and add them all together
+// Must use Rest Parameters (Rest Arguments)
+function sumAll(...theSums) {
+    let total = 0;
+    for (let arg of theSums) {
+        total += arg
+    }
+    return total;
+}
+
+console.log('sumAll', sumAll(1)) // 1
+console.log('sumAll', sumAll(1, 2)) // 3
+console.log('sumAll', sumAll(1, 2, 3)) // 6
+
+// Use the spread operator to pass arr into your sumAll function
+
+console.log('sumAll Spread', sumAll(...arr));
+
+//write a function combineArr() that accepts 2 arrays, and combines them into a single array using the spread operator
+
+function combineArr(arr1, arr2) {
+    return [...arr1, ...arr2]
+}
+
+let arrFirst = [1, 2, 3];
+let arrSecond = [4, 5, 6];
+
+console.log(combineArr(arrFirst, arrSecond)) // [1, 2, 3, 4, 5, 6]
+
+export {
+    sumTill,
+    makeTree,
+    between,
+    inArr,
+    sumAll,
+    combineArr
+}
