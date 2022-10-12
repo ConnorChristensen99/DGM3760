bff1997f4018b44723f3982b07f89c8e9d7a9d6c
ConnorChristensen99 <connor.christensen@uvu.edu>
2022-10-12 17:47:02 -0600

    added folder to the correct location to push

---
+++ myapp/client/src/main.js
@@ -0,0 +1,259 @@
+
+
+let todos = [
+    {
+        todoID: 0,
+        todoText: "Finish Homework",
+        todoComplete: false,
+        todoCategory: "School"
+    },
+    {
+        todoID: 1,
+        todoText: "Do Dishes", 
+        todoComplete: false,
+        todoCategory: "Home"
+    }, 
+    {
+        todoID: 2,
+        todoText: "Wash Car",
+        todoComplete: false,
+        todoCategory: "Extra Work"
+    }
+]
+async function getTodos() {
+    let response = await fetch('/todos')
+    let data = await response.json()
+
+    return data //Use the .then() to get the data
+}
+
+let listContainer = document.getElementById("todoList");
+let addBtn = document.querySelector('#addBtn')
+let todoInput = document.querySelector('#inputText')
+
+//Displays Todos
+function displayTodos(todos) {
+    listContainer.innerHTML = ""
+
+    todos.forEach(todo => {
+        let done = todo.todoComplete ? "done" : "";
+        let todoMarkup = `<div id="C${todo.todoID}><span id="categoryTitle">${todo.todoCategory}</span> <li id = 'T${todo.todoID}'>${todo.todoText}</li></div>`
+
+        listContainer.insertAdjacentHTML('beforeend', todoMarkup)
+    })
+    findtoDoLeft()
+}
+
+//Lets User add ToDos
+addBtn.addEventListener('click', event => {
+    let todoText = todoInput.value
+    addTodo(todoText)
+    displayTodos(todos)
+    findtoDoLeft()
+})
+
+function addTodo(todoText) {
+        fetch('/todos', {
+            method: 'POST',
+            body: JSON.stringify({todo: todoText, todoCategory: categoryHolder.value}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data =>  {
+            displayTodos(data)
+        })
+        findtoDoLeft()
+}
+
+//displays categories
+let categoryHolder = document.getElementById('category')
+let categories = ["School","Home","Extra Work"]
+
+function displayCategories() {
+    
+    categories.forEach(element => {
+
+        let categoryOption = document.createElement('option')
+        categoryOption.textContent = element
+    
+        categoryHolder.appendChild(categoryOption)
+    
+    }
+        
+    )
+   
+}
+displayCategories(categories)
+
+
+//Lets user add new categories
+let form = document.getElementById('categoryForm')
+function addCategory() {
+    form.classList.toggle('invisible')
+}
+
+let submitCategory = document.getElementById('submitCategory')
+let cname = document.getElementById('cname')
+
+
+submitCategory.addEventListener('click', () => {
+    let categoryText = cname.value
+    form.classList.toggle('invisible')
+    
+    let categoryOption = document.createElement('option')
+    categoryOption.textContent = categoryText
+    categories.push(categoryOption.value)
+
+    categoryHolder.appendChild(categoryOption)
+
+})
+
+//lets user edit categories for todos
+let form2 = document.getElementById('editcategoryForm')
+
+
+function editCategory() {
+    let newCategoryList = []
+    form2.innerHTML = ""
+    categoryHolder.innerText = ""
+
+
+    for (i=0;i<categories.length; i++) {
+        let todoCategories = `<span id='${i}'>${categories[i]}</span>`
+        form2.insertAdjacentHTML('beforeend', todoCategories)
+    }
+
+    form2.classList.toggle('invisible')
+
+    form2.addEventListener('dblclick', function handleClick(event) {
+        let target = event.target
+        targetID = target.id
+        
+        target.remove()
+        categories.splice(targetID, 1)
+    })
+    
+
+    if(form2.contentEditable = "false") {
+        form2.contentEditable = true;
+        let button = document.createElement('button');
+        button.innerText = "Save Changes";
+        button.classList.add('saveButton')
+        form2.appendChild(button);
+
+        button.addEventListener('click', (event) => {
+            form2.contentEditable = false;
+            button.remove()
+            
+        })
+}
+
+
+
+
+
+let submitbutton = document.createElement('button')
+submitbutton.innerText = 'Submit'
+form2.appendChild(submitbutton);
+
+submitbutton.addEventListener('click', () => {
+form2.classList.toggle('invisible')
+
+for (let i=0; i<categories.length; i++) {
+        let newCategoryq = document.getElementById(`${i}`).innerHTML
+        categories.splice(i, 1, newCategoryq)
+    
+}
+
+
+displayCategories(categories)
+
+})
+
+
+let formP = document.createElement('p')
+formP.textContent = "Double Click on a Category to remove it"
+  
+form2.appendChild(formP)
+
+}
+
+//Lets user click clear done button and remove objects
+function deleteCompleted() {
+    const todos = document.getElementsByClassName("done");
+    while(todos.length > 0){
+        todos[0].remove();
+    }
+
+    findtoDoLeft()
+
+    // fetch('/todos', {
+    //     method: 'DELETE',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(todos.classList.contains(done"))
+    // })
+    // .then(res => res.json())
+    // .then(data =>  {
+    //     displayTodos(data)
+    // })
+}
+
+
+//shows total amount of todos left. Added this function to call again in ever other function
+function findtoDoLeft(){
+    let listContainer = document.getElementById("todoList").getElementsByTagName('li').length;
+
+    numberLeft = document.getElementById('leftToDo')
+
+    numberLeft.textContent = `You have ${listContainer} remaining items left to be completed`
+}
+
+findtoDoLeft()
+
+
+
+//Lets the user complete and uncomplete tasks
+listContainer.addEventListener('dblclick', function handleClick(event) {
+    let target = event.target
+    
+    if (event.target.classList.contains('done')) {
+        target.classList.remove('done')
+    }
+    
+    else {
+        event.target.classList.add('done')
+    }
+})
+
+//Lets User edit todo
+
+function editList() {
+    if(listContainer.contentEditable = "false") {
+        listContainer.contentEditable = true;
+        let button = document.createElement('button');
+        button.innerText = "Save Changes";
+        button.classList.add('saveButton')
+        listContainer.appendChild(button);
+
+        button.addEventListener('click', (event) => {
+            listContainer.contentEditable = false;
+            button.remove()
+        })
+}}
+
+
+getTodos().then( todos => {
+    displayTodos(todos)
+})
+
+// for(let i=0; i<todos.length; i++) {
+//     let doneItems = document.getElementsByTagName('li')
+//     // if(li.classList.contains('done')) {
+//     //     console.log(li)
+//     // }
+//     console.log(doneItems)
+// }///Trying to get the done items so we can target those to remove them from the API
---
+++ myapp/index.js
@@ -0,0 +1,116 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8000
+
+app.use(express.static('client'))
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+let todos = [
+    {
+        todoID: 0,
+        todoText: "Finish Homework",
+        todoComplete: false,
+        todoCategory: "School"
+    },
+    {
+        todoID: 1,
+        todoText: "Do Dishes", 
+        todoComplete: false,
+        todoCategory: "Home"
+    }, 
+    {
+        todoID: 2,
+        todoText: "Wash Car",
+        todoComplete: false,
+        todoCategory: "Extra Work"
+    }
+]
+
+let categories = ["School","Home","Extra Work"]
+
+///////ToDos////////
+app.get('/todos', (req, res) => {//If I see a get request here then I will run this function
+    res.send(todos)
+})
+
+app.post('/todos', (req, res) => {//If I see a post request at this request then I will run this function
+    const todo = req.body.todo
+    const category = req.body.todoCategory
+    todos.push( {
+        todoID: todos.length,
+        todoText: todo,
+        todoComplete: false,
+        todoCategory: category
+    })
+
+    res.send(todos)
+
+})
+
+app.put('/todos', (req, res) => {
+    const todo = req.body.id
+    const todoText = req.body.todo
+
+    let tobeUpdated = todos.findIndex(todo)
+
+    todos.splice(tobeUpdated, 1, todoText)
+
+    res.send(todos)
+ 
+})
+
+
+
+app.delete('/todos', (req, res) => {
+    const todo = req.body.id
+    let tobeDeleted = todos.findIndex(todo)
+
+    todos.splice(tobeDeleted, 1)
+
+    res.send(todos)
+})
+
+///////Categories////////
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.post('/categories', (req, res) => {
+    const category = req.body.category
+    categories.push(category)
+
+    res.send(categories)
+
+})
+
+app.put('/categories', (req, res) => {
+    const category = req.body.id
+    const categoryText = req.body.category
+
+    let tobeUpdated = categories.findIndex(category)
+
+    categories.splice(tobeUpdated, 1, categoryText)
+
+
+    res.send(categories)
+ 
+})
+
+
+
+app.delete('/categories', (req, res) => {
+    const category = req.body.id
+    let tobeDeleted = categories.findIndex(category)
+
+    categories.splice(tobeDeleted, 1)
+
+    res.send(categories)
+})
+
+
+
+
