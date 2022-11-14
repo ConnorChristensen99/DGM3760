a9e7bcfa0b508aa863b5c4953d4f8da3cb4b5704
ConnorChristensen99 <connor.christensen@uvu.edu>
2022-11-14 16:43:46 -0700

    Lets user select amount of stars they want for reviews and display them

--- FullStackFinal/src/reviews.js
+++ FullStackFinal/src/reviews.js
@@ -1,13 +1,14 @@
 
 // Reviews JS
-// let reviews = [
-//     {
-//         reviewID: 0,
-//         title: 'Harry Potter and the Order of the Phoenix',
-//         description: 'Now in his fifth year at Hogwarts, Harry (Daniel Radcliffe) learns that many in the wizarding community do not know the truth of his encounter with Lord Voldemort.',
-//         image: "src/images/orderofthephoenix.jpg"
-//     }
-// ]
+let reviews = [
+    {
+        reviewID: 0,
+        title: 'Harry Potter and the Order of the Phoenix',
+        review: 'In their new book, Renee Dudley and Daniel Golden explain how a ragtag band of international tech nerds have defended the defenseless against cybercrime0. In their new book, Renee Dudley and Daniel Golden explain how a ragtag band of international tech nerds have defended the defenseless against cybercrime.',
+        image: "images/orderofthephoenix.jpg",
+        rating: 4
+    }
+]
 
 
 let searchForm = document.getElementById('search-form')
@@ -20,6 +21,36 @@ let bodyCards = document.getElementById('reviewCards')
 
 let starRating = document.getElementById('rating')
 let stars = starRating.getElementsByTagName('span')
+
+let reviewText = document.getElementById('reviewText')
+
+
+//Displays the reviews
+function displayReviews(reviews) {
+    bodyCards.innerHTML = ""
+
+    reviews.forEach(review => {
+        let reviewMarkup = `<div class="newReview"><img src=${review.image} class="reviewImage" alt="Image of the Book"> 
+        <div class="newReviewInfo"> <div id="stars-bottom"><h4>${review.title}</h4> <br>
+        <p>${review.review}</p></div>
+        <div class="rating">
+            <span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x checked"></span>
+            <span class="fa fa-star fa-2x "></span>
+                </div>
+                </div>
+            </div>`
+
+
+
+            bodyCards.insertAdjacentHTML('beforeend', reviewMarkup)
+    })
+
+}
+
+displayReviews(reviews)
 
 
 //Handles the Star Rating
@@ -37,10 +68,12 @@ for (let i=0; i<stars.length; i++) {
         let target = event.target
         targetID = target.id
 
-        ,
         selectStars(targetID)
+
        
 })}
+
+
 
 
 //Displays the Add A Review Form
@@ -54,10 +87,32 @@ removeBtn.addEventListener('click', event => {
     searchForm.classList.add('invisible')
 })
 
-//Adds a Review and removes the form
+//Adds a Review and removes the form  
 addReviewBtn.addEventListener('click', event => {
     searchForm.classList.add('invisible')
 
-})
+    let starIDArr = []
+    for (let i=0; i < stars.length; i++) {
+       let starID = (stars.item(i).classList.contains('checked'))
 
+       if(starID == true) {
+        starIDArr.push(starID)
+
+       }
+    }
+
+   
+
+
+    reviews.push({
+        reviewID: reviews.length,
+        title: 'Harry Potter and the Order of the Phoenix',
+        review: reviewText.value,
+        image: "images/orderofthephoenix.jpg",
+        rating: starIDArr.length
+    })                                                          //Just need to get this value to show the amount of selected stars
+
+    displayReviews(reviews)
+    
+})
 
